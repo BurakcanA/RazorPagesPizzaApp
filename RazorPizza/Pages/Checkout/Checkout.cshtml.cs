0e60c035e601699ff7f384f485f4a169e7d2542e
BurakcanA <burakcanaytac@gmail.com>
2023-04-05 21:32:26 +0300

    ModelAdded

---
+++ RazorPizza/Pages/Checkout/Checkout.cshtml.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPizza.Pages.Checkout
+{
+	public class CheckoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

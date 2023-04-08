a031e77e63876d35f1688cd2602266e98ce4b6f7
BurakcanA <burakcanaytac@gmail.com>
2023-04-08 17:22:33 +0300

    Data

--- RazorPizza/Program.cs
+++ RazorPizza/Program.cs
@@ -1,6 +1,12 @@
+using RazorPizza.Data;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefautlConnection"));
+});
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
